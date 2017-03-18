f6ff47e86da055bcd45de1d92476d0bd0615cd1c
Romain Buisson <romain@mekomsolutions.com>
2017-03-18 11:49:26 +0000

    INFRA-71: Update the symlink commands
    	modified:   bahmni_docker/resources/move-sources.sh

--- bahmni_docker/resources/move-sources.sh
+++ bahmni_docker/resources/move-sources.sh
@@ -5,12 +5,12 @@ sudo rm -r /opt/openmrs/modules
 sudo ln -s /mnt/modules /opt/openmrs/modules
 
 sudo mv /opt/openmrs/openmrs.war /mnt/war/
-sudo ln -s /mnt/war/openmrs.war /opt/openmrs/openmrs.war
+sudo ln -s /opt/openmrs/openmrs.war /mnt/war/openmrs.war
 
 sudo mv /opt/bahmni-web/etc/bahmni_config/* /mnt/bahmni_config/
 sudo rm -r /opt/bahmni-web/etc/bahmni_config
-sudo ln -s /mnt/bahmni_config /opt/bahmni-web/etc/bahmni_config
+sudo ln -s /opt/bahmni-web/etc/bahmni_config /mnt/bahmni_config
 
 sudo mv /opt/bahmni-web/etc/bahmniapps/* /mnt/bahmniapps/
 sudo rm -r /opt/bahmni-web/etc/bahmniapps
-sudo ln -s /mnt/bahmniapps /opt/bahmni-web/etc/bahmniapps
+sudo ln -s /opt/bahmni-web/etc/bahmniapps /mnt/bahmniapps
