c72b2de01d248152760e064cc219b9b0a0d32db1
mksrom <romain@mekomsolutions.com>
2017-04-14 10:53:51 +0200

    INFRA-71: Add the OpenMRS Config folder as a dev folder (/opt/openmrs/configuration)
    	modified:   bahmni_docker/resources/link-sources.sh

--- bahmni_docker/resources/link-sources.sh
+++ bahmni_docker/resources/link-sources.sh
@@ -1,13 +1,21 @@
 #!/bin/bash
 
+# Modules directory
 sudo rm -r /opt/openmrs/modules
 sudo ln -s /mnt/modules /opt/openmrs/modules
 
+# openmrs.war
 sudo rm /opt/openmrs/openmrs.war
 sudo ln -s /mnt/war/openmrs.war /opt/openmrs/openmrs.war
 
+# Bahmni config
 sudo rm -r /opt/bahmni-web/etc/bahmni_config
 sudo ln -s /mnt/bahmni_config /opt/bahmni-web/etc/bahmni_config
 
+# OpenMRS config
+sudo rm -r /opt/openmrs/configuration
+sudo ln -s /mnt/openmrs_config /opt/openmrs/configuration
+
+# Bahmni Apps
 sudo rm -r /opt/bahmni-web/etc/bahmniapps
 sudo ln -s /mnt/bahmniapps /opt/bahmni-web/etc/bahmniapps
