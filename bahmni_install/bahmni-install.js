4c088ab19830d167cf3f791d8260e8a22b4cfa24
mksrom <romain@mekomsolutions.com>
2017-04-15 15:11:28 +0200

    INFRA-71: NodeJS scripts for the 'bahmni-install' Jenkins job
    	new file:   bahmni_install/bahmni-install.js
    	new file:   bahmni_install/get_module_version.js

---
+++ bahmni_install/bahmni-install.js
@@ -0,0 +1,48 @@
+var fs = require('fs');
+
+var repo = process.env.repo;
+var branch = process.env.branch;
+
+var envvars = "";
+
+var project = "";
+var configType = "";
+var distro = "";
+var server = "";
+
+// For the specific case of '...-config-...'like repos, the Jenkins project name is not equal to the repo name.
+// Bahmni Config jenkins job is generic but the repo is distribution specific.
+if (repo.match("-config-") == "-config-") {
+  project = process.env.configProject;
+  repoSplit = repo.split("-");
+  configType = repoSplit[0];
+  distro = repoSplit[repoSplit.length - 1];
+} else if (repo.match("bahmniapps") == "bahmniapps") {
+  project = process.env.bahmniappsProject;
+} else {
+  // if project is not provided as input parameter, ie, the job is triggered by a parent project
+  if (project == "") {
+    project = process.env.openmrsModuleProject
+  }
+}
+
+// If branch is "master", server is "test", otherwise, the server is the branch name
+if (branch == "master") {
+  server = "test";
+} else {
+  server = branch;
+}
+
+envvars = envvars +  "project=" + project + "\n";
+envvars = envvars +  "configType=" + configType + "\n";
+envvars = envvars +  "distro=" + distro + "\n";
+envvars = envvars +  "server=" + server + "\n";
+envvars = envvars +  "branch=" + branch + "\n";
+console.log(envvars);
+
+// Export the environment variables
+fs.appendFile(process.env.WORKSPACE + "/envvars", envvars, function(err) {
+  if(err) {
+    return console.log(err);
+  }
+}); 
---
+++ bahmni_install/get_module_version.js
@@ -0,0 +1,45 @@
+/* This script is WIP and is supposed to extract the current module version, to then pass it to the 'distro_compare_module_version' */
+/* TODO: Should implement a module specific behavior, with a specific fallback behavior on pom.xml style fetch */
+
+// If no 'distro' is provided as parameter then go fetch the distributions that are concerned by this module
+if (! process.env.distro) {
+  fs = require('fs');
+
+  var envvars = "";
+  var moduleName = process.env.repo;
+  var moduleNameSplit = moduleName.split('-');
+  var moduleShortName = moduleNameSplit[moduleNameSplit.length - 1];
+  var skipVersion = false;
+
+  // For now, we only call the distro_compare_module_version with a skipVersion=true parameter
+  // This will return the distributions that have dependency on the module, whatever the version
+  skipVersion = true;
+  var moduleVersion = getModuleVersion(moduleName);
+  exportVars();
+
+  /* TODO: Should implement a module specific behavior */
+  function getModuleVersion (moduleName) {
+    var version = "";
+    if (moduleName == "openmrs-module-bahmniapps") {
+      // Bahmni Apps is not a maven project. No pom.xml.
+      // Implement Bahmni Apps specfic behavior to retrieve its curent version
+      // TODO: retreive the Bahmni Apps module version --
+    } else {
+      // TODO: implement the default behavior, ie, fetch the pom.xml and read the version
+    }
+    return version;
+  }
+
+  /* Export environment variables */
+  function exportVars () {
+    envvars = envvars +  "moduleName=" + moduleName + "\n";
+    envvars = envvars +  "moduleShortName=" + moduleShortName + "\n";
+    envvars = envvars +  "moduleVersion=" + moduleVersion + "\n";
+    envvars = envvars +  "skipVersion=" + skipVersion + "\n";
+    fs.appendFile(process.env.WORKSPACE + "/envvars", envvars, function(err) {
+      if(err) {
+        return console.log(err);
+      }
+    });
+  }
+}
