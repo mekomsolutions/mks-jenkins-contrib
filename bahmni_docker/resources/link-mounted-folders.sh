2523cc7e3bac2d4d769a21e2bf7b184922a21a23
rbuisson <romain@mekomsolutions.com>
2018-01-04 11:41:47 +0100

    Add the OWA folder mapping
    	modified:   bahmni_docker/resources/link-mounted-folders.sh

--- bahmni_docker/resources/link-mounted-folders.sh
+++ bahmni_docker/resources/link-mounted-folders.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
-# Modules directory
-rm -r /opt/openmrs/modules
+# OpenMRS modules directory
+rm -rf /opt/openmrs/modules
 ln -s /mnt/modules /opt/openmrs/modules
 chown -h bahmni:bahmni /opt/openmrs/modules
+
+# OWA directory
+rm -rf /opt/openmrs/owa
+ln -s /mnt/owa /opt/openmrs/owa
+chown -h bahmni:bahmni /opt/openmrs/owa
 
 # openmrs.war
 rm /opt/openmrs/openmrs.war
@@ -11,22 +16,22 @@ ln -s /mnt/war/openmrs.war /opt/openmrs/openmrs.war
 chown -h bahmni:bahmni /opt/openmrs/openmrs.war
 
 # Bahmni config
-rm -r /opt/bahmni-web/etc/bahmni_config
+rm -rf /opt/bahmni-web/etc/bahmni_config
 ln -s /mnt/bahmni_config /opt/bahmni-web/etc/bahmni_config
 chown -h bahmni:bahmni /opt/openmrs/bahmni_config
 
 # OpenMRS config
-rm -r /opt/openmrs/configuration
+rm -rf /opt/openmrs/configuration
 ln -s /mnt/openmrs_config /opt/openmrs/configuration
 chown -h bahmni:bahmni /opt/openmrs/configuration
 
 # Bahmni Apps
-rm -r /opt/bahmni-web/etc/bahmniapps
+rm -rf /opt/bahmni-web/etc/bahmniapps
 ln -s /mnt/bahmniapps /opt/bahmni-web/etc/bahmniapps
 chown -h bahmni:bahmni /opt/bahmni-web/etc/bahmniapps
 
 # Logs
-rm -r /mnt/logs/openmrs.log
+rm -rf /mnt/logs/openmrs.log
 ln -s /opt/openmrs/openmrs.log /mnt/logs/openmrs.log
 
 # Ensure correct access rights
