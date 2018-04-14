f4e1f010b9a507dbacbc703c864510220169d9ed
rbuisson <romain@mekomsolutions.com>
2018-04-14 14:41:49 +0200

    INFRA-117: Add support 'bahmni_home' mounted folder
    	modified:   bahmni_docker/resources/link-mounted-folders.sh

--- bahmni_docker/resources/link-mounted-folders.sh
+++ bahmni_docker/resources/link-mounted-folders.sh
@@ -3,45 +3,51 @@
 # OpenMRS modules directory
 rm -rf /opt/openmrs/modules
 ln -s /mnt/modules /opt/openmrs/modules
-chown -h bahmni:bahmni /opt/openmrs/modules
+chown -R bahmni:bahmni /mnt/modules
 
 # OWA directory
 rm -rf /opt/openmrs/owa
 ln -s /mnt/owa /opt/openmrs/owa
-chown -h bahmni:bahmni /opt/openmrs/owa
+chown -R bahmni:bahmni /mnt/owa
 
 # openmrs.war
-rm /opt/openmrs/openmrs.war
+rm -rf /opt/openmrs/openmrs.war
 ln -s /mnt/war/openmrs.war /opt/openmrs/openmrs.war
-chown -h bahmni:bahmni /opt/openmrs/openmrs.war
+chown -R bahmni:bahmni /mnt/war/openmrs.war
 
 # Bahmni config
 rm -rf /opt/bahmni-web/etc/bahmni_config
 ln -s /mnt/bahmni_config /opt/bahmni-web/etc/bahmni_config
-chown -h bahmni:bahmni /opt/openmrs/bahmni_config
+chown -R bahmni:bahmni /mnt/bahmni_config
 
 # OpenMRS config
 rm -rf /opt/openmrs/configuration
 ln -s /mnt/openmrs_config /opt/openmrs/configuration
-chown -h bahmni:bahmni /opt/openmrs/configuration
+chown -R bahmni:bahmni /mnt/openmrs_config
 
 # Bahmni Apps
 rm -rf /opt/bahmni-web/etc/bahmniapps
 ln -s /mnt/bahmniapps /opt/bahmni-web/etc/bahmniapps
-chown -h bahmni:bahmni /opt/bahmni-web/etc/bahmniapps
+chown -R bahmni:bahmni /mnt/bahmniapps
 
 # Bahmni Connect
 rm -rf /opt/bahmni-offline/bahmni-connect-apps
 ln -s /mnt/bahmni_connect /opt/bahmni-offline/bahmni-connect-apps
-chown -h bahmni:bahmni /opt/bahmni-offline/bahmni-connect-apps
+chown -R bahmni:bahmni /mnt/bahmni_connect
 
-# Backups
+# Database dump
 ln -s /mnt/data/db_dumps /data
+chown -R bahmni:bahmni /mnt/data/db_dumps
+
+# Bahmni Home
+rm -rf /home/bahmni
+ln -s /mnt/data/bahmni_home /home/bahmni
+chown -R bahmni:bahmni /mnt/data/bahmni_home
 
 # Logs
-rm -rf /mnt/logs/openmrs.log
-ln -s /opt/openmrs/openmrs.log /mnt/logs/openmrs.log
+mv -f /opt/openmrs/openmrs.log /mnt/logs/
+chown -R bahmni:bahmni /mnt/logs/openmrs.log
+ln -s /mnt/logs/openmrs.log /opt/openmrs/
 
-# Ensure correct access rights
-sudo chown -R bahmni:bahmni /mnt/
-sudo chown -R mysql:mysql /mnt/data/mysql_datadir
+# Ensure MySQL datadir access rights
+chown -R mysql:mysql /mnt/data/mysql_datadir
